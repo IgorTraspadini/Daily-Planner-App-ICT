50bcff7bdab1e0b693da1a47ab6f771b59a8e247
IgorTraspadini <igorchieppe@hotmail.com>
2024-01-09 10:13:23 -0300

    Grid CSS and Jquery done!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 const currentDay = $("#currentDay");
+const ElemDiv = $(".container-fluid");
 
-
-
+const timeArray = ["8AM", "9AM", "10AM", "11AM",
+  "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
 
 currentDay.text(dayjs().format("dddd, MMMM D"));
+$.each(timeArray, function (i, v) {
+    let Row = $("<div>").addClass("row");
+    let Time = $("<div>").addClass("hour col-sm-2 col-lg-1");
+    let Descr = $("<textarea>").addClass("description col-sm col-lg");
+    let bt = $("<div>").addClass("saveBtn col-sm-2 col-lg-1");
+    Time.html('<br><br>' + v);
+    Row.attr("data-time",v);
+//   Time.text(v);
+//    bt.text("X");
+    Row.append(Time, Descr, bt);
+    ElemDiv.append(Row);
+
+})
